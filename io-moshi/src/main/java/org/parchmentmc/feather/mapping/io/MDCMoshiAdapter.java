1aaac6d34c038b630dd5cac63b7b935b082351dc
SciWhiz12 <arnoldnunag12@gmail.com>
2021-05-12 07:48:11 +0800

    Add small javadoc to MDCMoshiAdapter

--- io-moshi/src/main/java/org/parchmentmc/feather/mapping/io/MDCMoshiAdapter.java
+++ io-moshi/src/main/java/org/parchmentmc/feather/mapping/io/MDCMoshiAdapter.java
@@ -11,6 +11,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Moshi adapter for {@link MappingDataContainer}s and its inner data classes.
+ */
 public class MDCMoshiAdapter {
     private final boolean ignoreNonDocumented;
 
