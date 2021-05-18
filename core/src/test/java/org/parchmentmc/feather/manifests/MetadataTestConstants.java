6d45dd762bf8caa2c6a77c85e2353f5ac27a6bdc
SciWhiz12 <arnoldnunag12@gmail.com>
2021-05-18 09:24:50 +0800

    Move metadata tests data into separate superinterface
    This allows the use of the test data in other places, such as tests in the
    io-moshi subproject.

---
+++ core/src/test/java/org/parchmentmc/feather/manifests/MetadataTestConstants.java
@@ -0,0 +1,81 @@
+package org.parchmentmc.feather.manifests;
+
+import org.parchmentmc.feather.metadata.*;
+import org.parchmentmc.feather.util.SimpleVersion;
+
+import java.lang.reflect.Modifier;
+import java.util.List;
+
+import static com.google.common.collect.ImmutableList.of;
+import static org.parchmentmc.feather.manifests.TestConstantsHelper.named;
+
+public interface MetadataTestConstants {
+    List<MethodReference> METHOD_REFERENCES = of(
+            new ImmutableMethodReference(named("a"), named("<init>"),
+                    named("()V"), named("()V")),
+            new ImmutableMethodReference(named("a"), named("ma"),
+                    named("()Ljava/util/List;"), named("()Ljava/util/List<[I>;"))
+    );
+
+    List<MethodMetadata> METHOD_METADATA = of(
+            new ImmutableMethodMetadata(named("a"), false, null, of(),
+                    named("ma"), Modifier.PROTECTED, named("()Ljava/util/List;"), named("()Ljava/util/List<[I>;")),
+            new ImmutableMethodMetadata(named("a"), true, null, METHOD_REFERENCES,
+                    named("la"), Modifier.PROTECTED, named("()F"), named("()F")),
+            new ImmutableMethodMetadata(named("a"), false, METHOD_REFERENCES.get(0), of(),
+                    named("xa"), Modifier.PROTECTED, named("()V"), named("()V"))
+    );
+
+    List<FieldMetadata> FIELD_METADATA = of(
+            new ImmutableFieldMetadata(
+                    named("a"),
+                    named("aa", "testField"),
+                    Modifier.PUBLIC | Modifier.STATIC,
+                    named("F"),
+                    named("F")
+            )
+            , new ImmutableFieldMetadata(
+                    named("a"),
+                    named("bb"),
+                    Modifier.PUBLIC,
+                    named("Ljava/util/List;", "Ljava/util/List;"),
+                    named("Ljava/util/List<[I>;", "Ljava/util/List<[I>;")
+            )
+    );
+
+    List<ClassMetadata> INNER_CLASSES = of(
+            new ImmutableClassMetadata(named("a"),
+                    of(named("b"), named("c", "IInterface")),
+                    METHOD_METADATA, FIELD_METADATA, of(),
+                    null, named("d"),
+                    Modifier.PUBLIC | Modifier.FINAL
+            ),
+            new ImmutableClassMetadata(null,
+                    of(),
+                    METHOD_METADATA, FIELD_METADATA, of(),
+                    null, named("e"),
+                    Modifier.PUBLIC | Modifier.FINAL
+            )
+    );
+
+    List<ClassMetadata> CLASS_METADATA = of(
+            new ImmutableClassMetadata(named("f"),
+                    of(named("h"), named("c", "IInterface")),
+                    METHOD_METADATA, FIELD_METADATA, INNER_CLASSES,
+                    null, named("g"),
+                    Modifier.PUBLIC
+            ),
+            new ImmutableClassMetadata(null,
+                    of(),
+                    METHOD_METADATA, FIELD_METADATA, of(),
+                    null, named("h"),
+                    Modifier.PUBLIC
+            )
+    );
+
+    List<SourceMetadata> SOURCE_METADATA = of(
+            new ImmutableSourceMetadata(SimpleVersion.of(1, 0, 0), "1.16.5", CLASS_METADATA),
+            new ImmutableSourceMetadata(SimpleVersion.of("1.0.0"), "21w01a", of())
+    );
+}
+
--- io-gson/src/test/java/org/parchmentmc/feather/io/gson/MetadataAdapterFactoryTest.java
+++ io-gson/src/test/java/org/parchmentmc/feather/io/gson/MetadataAdapterFactoryTest.java
@@ -1,97 +1,12 @@
 package org.parchmentmc.feather.io.gson;
 
-import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
 import org.parchmentmc.feather.io.gson.metadata.MetadataAdapterFactory;
+import org.parchmentmc.feather.manifests.MetadataTestConstants;
 import org.parchmentmc.feather.metadata.*;
-import org.parchmentmc.feather.named.ImmutableNamed;
-import org.parchmentmc.feather.named.Named;
-import org.parchmentmc.feather.util.Constants;
 import org.parchmentmc.feather.util.SimpleVersion;
 
-import java.lang.reflect.Modifier;
-import java.util.List;
-
-import static com.google.common.collect.ImmutableList.of;
-
-public class MetadataAdapterFactoryTest extends GSONTest {
-
-    private static Named named(String value) {
-        return new ImmutableNamed(Constants.Names.OBFUSCATED, value);
-    }
-
-    private static Named named(String valueA, String valueB) {
-        return new ImmutableNamed(ImmutableMap.of(Constants.Names.OBFUSCATED, valueA, Constants.Names.MOJANG, valueB));
-    }
-
-    private static final List<MethodReference> METHOD_REFERENCES = of(
-            new ImmutableMethodReference(named("a"), named("<init>"),
-                    named("()V"), named("()V")),
-            new ImmutableMethodReference(named("a"), named("ma"),
-                    named("()Ljava/util/List;"), named("()Ljava/util/List<[I>;"))
-    );
-
-    private static final List<MethodMetadata> METHOD_METADATA = of(
-            new ImmutableMethodMetadata(named("a"), false, null, of(),
-                    named("ma"), Modifier.PROTECTED, named("()Ljava/util/List;"), named("()Ljava/util/List<[I>;")),
-            new ImmutableMethodMetadata(named("a"), true, null, METHOD_REFERENCES,
-                    named("la"), Modifier.PROTECTED, named("()F"), named("()F")),
-            new ImmutableMethodMetadata(named("a"), false, METHOD_REFERENCES.get(0), of(),
-                    named("xa"), Modifier.PROTECTED, named("()V"), named("()V"))
-    );
-
-    private static final List<FieldMetadata> FIELD_METADATA = of(
-            new ImmutableFieldMetadata(
-                    named("a"),
-                    named("aa", "testField"),
-                    Modifier.PUBLIC | Modifier.STATIC,
-                    named("F"),
-                    named("F")
-            )
-            , new ImmutableFieldMetadata(
-                    named("a"),
-                    named("bb"),
-                    Modifier.PUBLIC,
-                    named("Ljava/util/List;", "Ljava/util/List;"),
-                    named("Ljava/util/List<[I>;", "Ljava/util/List<[I>;")
-            )
-    );
-
-    private static final List<ClassMetadata> INNER_CLASSES = of(
-            new ImmutableClassMetadata(named("a"),
-                    of(named("b"), named("c", "IInterface")),
-                    METHOD_METADATA, FIELD_METADATA, of(),
-                    null, named("d"),
-                    Modifier.PUBLIC | Modifier.FINAL
-            ),
-            new ImmutableClassMetadata(null,
-                    of(),
-                    METHOD_METADATA, FIELD_METADATA, of(),
-                    null, named("e"),
-                    Modifier.PUBLIC | Modifier.FINAL
-            )
-    );
-
-    private static final List<ClassMetadata> CLASS_METADATA = of(
-            new ImmutableClassMetadata(named("f"),
-                    of(named("h"), named("c", "IInterface")),
-                    METHOD_METADATA, FIELD_METADATA, INNER_CLASSES,
-                    null, named("g"),
-                    Modifier.PUBLIC
-            ),
-            new ImmutableClassMetadata(null,
-                    of(),
-                    METHOD_METADATA, FIELD_METADATA, of(),
-                    null, named("h"),
-                    Modifier.PUBLIC
-            )
-    );
-
-    private static final List<SourceMetadata> SOURCE_METADATA = of(
-            new ImmutableSourceMetadata(SimpleVersion.of(1, 0, 0), "1.16.5", CLASS_METADATA),
-            new ImmutableSourceMetadata(SimpleVersion.of("1.0.0"), "21w01a", of())
-    );
-
+public class MetadataAdapterFactoryTest extends GSONTest implements MetadataTestConstants {
     public MetadataAdapterFactoryTest() {
         super(b -> b.registerTypeAdapter(SimpleVersion.class, new SimpleVersionAdapter())
                 .registerTypeAdapterFactory(new MetadataAdapterFactory()));
