8b94155bb74fcc0614827d75245e152dab759efc
Marc Hermans <marc.hermans@ldtteam.com>
2021-06-05 17:54:50 +0200

    Fix the type and signature remapping and add complex test cases for it.

--- utils/src/main/java/org/parchmentmc/feather/utils/RemapHelper.java
+++ utils/src/main/java/org/parchmentmc/feather/utils/RemapHelper.java
@@ -1,6 +1,5 @@
 package org.parchmentmc.feather.utils;
 
-import java.util.Arrays;
 import java.util.function.UnaryOperator;
 
 /**
@@ -44,7 +43,10 @@ public final class RemapHelper {
 
         if (typeDescriptor.startsWith("L")) { // Reference type descriptor
             if (typeDescriptor.contains("<") && typeDescriptor.contains(">")) {
-                final String remappedTypeName = remappingFunction.apply(typeDescriptor.substring(1, typeDescriptor.indexOf("<")));
+                String remappedTypeName = remappingFunction.apply(typeDescriptor.substring(1, typeDescriptor.indexOf("<")));
+                if (remappedTypeName == null)
+                    remappedTypeName = typeDescriptor.substring(1, typeDescriptor.indexOf("<"));
+
                 final String genericArguments = typeDescriptor.substring(
                   typeDescriptor.indexOf("<") + 1,
                   typeDescriptor.lastIndexOf(">")
@@ -71,7 +73,25 @@ public final class RemapHelper {
                             c = genericArguments.charAt(++charIndex);
                         } while (c != ';' || openBrackets > 0);
 
-                        remappedGenericArgument.append(remapTypeDescriptor(refType.append(';').toString(), remappingFunction));
+                        final String originalGenericTypeArg = refType.append(';').toString();
+                        final String remappedGenericTypeArg =  remapTypeDescriptor(originalGenericTypeArg, remappingFunction);
+                        remappedGenericArgument.append(remappedGenericTypeArg);
+                    } else if (c == 'T') {
+                        //Generic type reference. Don't remap.
+                        StringBuilder refType = new StringBuilder();
+                        int openBrackets = 0;
+                        do {
+                            refType.append(c);
+                            if (c == '<') {
+                                openBrackets++;
+                            }else if (c == '>') {
+                                openBrackets--;
+                            }
+
+                            c = genericArguments.charAt(++charIndex);
+                        } while (c != ';' || openBrackets > 0);
+
+                        remappedGenericArgument.append(refType.append(';'));
                     } else {
                         throw new IllegalArgumentException("Unknown descriptor " + c + " in generic descriptor: " + typeDescriptor);
                     }
@@ -107,28 +127,44 @@ public final class RemapHelper {
         // (<parameter type descriptors>)<return type descriptor>
         int cursor = 0;
 
+        char c = ' ';
+        StringBuilder remappedOutput = new StringBuilder();
+        if (methodDescriptor.charAt(cursor) == '<') {
+            cursor++;
+            c = methodDescriptor.charAt(cursor);
+            final ExtractionResult result = extractGenericSection(methodDescriptor,++cursor,c);
+            final StringBuilder refType = result.getResult();
+            cursor = result.getCursor();
+            remappedOutput.append('<');
+            int genericCursor = 0;
+            char genericC = refType.charAt(genericCursor++);
+            do {
+                final ExtractionResult genericSectionRes = extractTypeName(refType.toString(), genericCursor, genericC);
+                final StringBuilder genericSection = genericSectionRes.getResult();
+                genericCursor = genericSectionRes.getCursor();
+
+                if (genericSection.charAt(1) == ':') {
+                    remappedOutput.append(genericSection.charAt(0));
+                    remappedOutput.append(":");
+                }
+                remappedOutput.append(remapTypeDescriptor(genericSection.substring(2) + ";", remappingFunction));
+            } while (genericCursor != refType.length());
+            remappedOutput.append('>');
+        }
+
         if (methodDescriptor.charAt(cursor++) != '(' || !methodDescriptor.contains(")")) {
             throw new IllegalArgumentException("Invalid method descriptor: " + methodDescriptor);
         }
-        StringBuilder remappedOutput = new StringBuilder().append('(');
 
-        char c;
+        remappedOutput.append('(');
+
         while ((c = methodDescriptor.charAt(cursor++)) != ')') {
             if (PRIMITIVE_TYPE_DESCRIPTORS.indexOf(c) > -1 || c == '[') {
                 remappedOutput.append(c);
             } else if (c == 'L') {
-                StringBuilder refType = new StringBuilder();
-                int openBrackets = 0;
-                do {
-                    refType.append(c);
-                    if (c == '<') {
-                        openBrackets++;
-                    }else if (c == '>') {
-                        openBrackets--;
-                    }
-
-                    c = methodDescriptor.charAt(cursor++);
-                } while (c != ';' || openBrackets > 0);
+                final ExtractionResult result = extractTypeName(methodDescriptor, cursor, c);
+                final StringBuilder refType = result.getResult();
+                cursor = result.getCursor();
                 remappedOutput.append(remapTypeDescriptor(refType.append(';').toString(), remappingFunction));
             } else {
                 throw new IllegalArgumentException("Unknown descriptor " + c + " in method descriptor: " + methodDescriptor);
@@ -142,5 +178,55 @@ public final class RemapHelper {
         remappedOutput.append(')').append(remapTypeDescriptor(returnType, remappingFunction));
 
         return remappedOutput.toString();
+    }
+
+    private static ExtractionResult extractTypeName(final String methodDescriptor, int cursor, char c)
+    {
+        final char terminationChar = ';';
+        return extractSection(methodDescriptor, cursor, c, terminationChar);
+    }
+
+    private static ExtractionResult extractGenericSection(final String methodDescriptor, int cursor, char c)
+    {
+        final char terminationChar = '>';
+        return extractSection(methodDescriptor, cursor, c, terminationChar);
+    }
+
+    private static ExtractionResult extractSection(final String methodDescriptor, int cursor, char c, final char terminationChar)
+    {
+        StringBuilder refType = new StringBuilder();
+        int openBrackets = 0;
+        do {
+            refType.append(c);
+            if (c == '<') {
+                openBrackets++;
+            }else if (c == '>') {
+                openBrackets--;
+            }
+
+            c = methodDescriptor.charAt(cursor++);
+        } while (c != terminationChar || openBrackets > 0);
+
+        return new ExtractionResult(refType, cursor);
+    }
+
+    private static final class ExtractionResult {
+        private final StringBuilder result;
+        private final int cursor;
+
+        private ExtractionResult(final StringBuilder result, final int cursor) {
+            this.result = result;
+            this.cursor = cursor;
+        }
+
+        public StringBuilder getResult()
+        {
+            return result;
+        }
+
+        public int getCursor()
+        {
+            return cursor;
+        }
     }
 }
